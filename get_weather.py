31461214f5ba14bf10c6d2ac73876ec97b73109d
Ruslan <onishruslan@yandex.ru>
2024-07-17 15:31:42 +0300

    В запрос к API добавлены погодные условия и влажность

--- get_weather.py
+++ get_weather.py
@@ -1,5 +1,37 @@
 import requests
 
+weather_codes = {
+    '0': 'Солнечно',
+    '1': 'Преимущественно ясно',
+    '2': 'Переменная облачность',
+    '3': 'Пасмурно',
+    '45': 'Туман',
+    '48': 'Оседающая изморозь',
+    '51': 'Морось: слабая',
+    '53': 'Морось: умеренная',
+    '55': 'Морось: интенсивная',
+    '56': 'Замерзающая морось: слабая',
+    '57': 'Замерзающая морось: сильная',
+    '61': 'Дождь: слабый',
+    '63': 'Дождь: умеренный',
+    '65': 'Дождь: сильный',
+    '66': 'Небольшой град',
+    '67': 'Сильный град',
+    '71': 'Снегопад: слабый',
+    '73': 'Снегопад: умеренный',
+    '75': 'Снегопад: сильный',
+    '77': 'Снежные зерна',
+    '80': 'Слабый ливень',
+    '81': 'Умеренный ливень',
+    '82': 'Сильный ливень',
+    '85': 'Снежные ливни: слабые',
+    '86': 'Снежные ливни: сильные',
+    '95': 'Гроза: слабая или умеренная',
+    '96': 'Гроза с небольшим градом',
+    '99': 'Гроза с сильным градом'
+}
+
+
 def get_coordinates(city):
 	nominatim_url = f'https://nominatim.openstreetmap.org/search?q={city}&format=json'
 	headers = {
@@ -16,16 +48,20 @@ def get_coordinates(city):
 def get_weather(city):
 	coordinates = get_coordinates('Москва')
 
-	api_url = f'https://api.open-meteo.com/v1/forecast?latitude={coordinates[0]}&longitude={coordinates[1]}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,pressure_msl,wind_speed_10m,wind_direction_10m&timezone=Europe%2FMoscow'
+	api_url = f'https://api.open-meteo.com/v1/forecast?latitude={coordinates[0]}&longitude={coordinates[1]}&hourly=weather_code,temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,pressure_msl,wind_speed_10m,wind_direction_10m&timezone=Europe%2FMoscow'
 	response = requests.get(api_url)
 
 	if response.status_code == 200:
 		data = response.json()
-		return converted_data(data)
+		return convert_data(data)
 
-def converted_data(weather):
+def convert_data(weather):
 	weather_data = []
 	for i in range(len(weather['hourly']['time'])):
+		weather_code = weather['hourly']['weather_code'][i]
+		weather_description = weather_codes[str(weather_code)]
+
+
 		data_dict = {
 			'time': weather['hourly']['time'][i],  # Время
 			'temperature': weather['hourly']['apparent_temperature'][i],  # Температура по ощущениям
@@ -34,7 +70,9 @@ def converted_data(weather):
 			'pressure_msl': weather['hourly']['pressure_msl'][i],  # Давление гПа
 			'temperature_fact': weather['hourly']['temperature_2m'][i],  # Температура фактическая
 			'wind_direction': weather['hourly']['wind_direction_10m'][i],  # Направление ветра в градусах
-			'wind_speed': weather['hourly']['wind_speed_10m'][i]  # Скорость ветра
+			'wind_speed': weather['hourly']['wind_speed_10m'][i],  # Скорость ветра'
+			'relative_humidity': weather['hourly']['relative_humidity_2m'][i],   #Влажность  %
+			'weather': weather_description    # Погода (солнечно, ливень и тд)
 		}
 		weather_data.append(data_dict)
-	return  weather_data
+	return weather_data
